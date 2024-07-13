b09583a1c390170b8117d54bce941357c988d597
Alok Kumar <kumaralokbatham@gmail.com>
2024-07-13 11:55:29 +0530

    formatted files

--- src/index.js
+++ src/index.js
@@ -1,19 +1,18 @@
-import _ from 'lodash';
-import './style.css';
+import _ from "lodash";
+import "./style.css";
 
 function component() {
-    const element = document.createElement('div');
-    // Lodash, now imported by this script
-    element.innerHTML = _.join(['Hello', 'Alok'], ' ');
-    element.classList.add('hello');
-    return element;
-  }
+  const element = document.createElement("div");
+  // Lodash, now imported by this script
+  element.innerHTML = _.join(["Hello", "Alok"], " ");
+  element.classList.add("hello");
+  return element;
+}
 
+function myFunc() {
+  console.log("Hi I am Alok");
+}
 
-  function myFunc( ) {
-    console.log("Hi I am Alok")
-  }
+myFunc();
 
-  myFunc();
-  
-  document.body.appendChild(component());
+document.body.appendChild(component());
--- webpack.config.js
+++ webpack.config.js
@@ -1,41 +1,41 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  mode: 'development',
+  mode: "development",
   entry: {
     index: {
-      import: './src/index.js',
-      dependOn: 'shared',
+      import: "./src/index.js",
+      dependOn: "shared",
     },
-    shared: 'lodash',
-   },
-  devtool: 'inline-source-map',
+    shared: "lodash",
+  },
+  devtool: "inline-source-map",
   devServer: {
-    static: './dist',
+    static: "./dist",
   },
   plugins: [
     new HtmlWebpackPlugin({
-        title: 'Development',
+      title: "Development",
     }),
   ],
   output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
     clean: true,
   },
   optimization: {
-    runtimeChunk: 'single',
+    runtimeChunk: "single",
   },
   module: {
     rules: [
       {
         test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
+        use: ["style-loader", "css-loader"],
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
+        type: "asset/resource",
       },
     ],
   },
